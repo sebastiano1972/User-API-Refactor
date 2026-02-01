3a0079cac2aceb98020f498c70aefdc0a5dd89ab
Sebastiano Serri <sebastiano1972@yahoo.it>
2026-02-01 10:32:39 +0000

    Added more data,

--- Tests.User.Api/Tests.User.Api/Extensions/HostExtensions.cs
+++ Tests.User.Api/Tests.User.Api/Extensions/HostExtensions.cs
@@ -10,24 +10,88 @@ internal static class HostExtensions
         var userRepository = unitOfWork.GetRepository<Domain.Entities.User>();
         var bookRepository = unitOfWork.GetRepository<Book>();
 
-        userRepository.Add(new Domain.Entities.User
-                           {
-                               FirstName = "Sebastiano",
-                               LastName = "Serri",
-                               Age = 54
-                           });
+        var user1 = userRepository.Add(new Domain.Entities.User
+                                       {
+                                           FirstName = "Sebastiano",
+                                           LastName = "Serri",
+                                           Age = 54
+                                       });
 
-        bookRepository.Add(new Book
-                           {
-                               Title = "Dune",
-                               Author = "Frank Herbert"
-                           });
+        var user2 = userRepository
+           .Add(new Domain.Entities.User
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Age = 54
+                });
 
-        bookRepository.Add(new Book
-                           {
-                               Title = "The heretics of Dune",
-                               Author = "Frank Herbert"
-                           });
+        var user3 = userRepository
+           .Add(new Domain.Entities.User
+                {
+                    FirstName = "Jane",
+                    LastName = "Doe",
+                    Age = 54
+                });
+
+        var book1 = bookRepository
+           .Add(new Book
+                {
+                    Title = "Dune",
+                    Author = "Frank Herbert",
+                    Comments =
+                    [
+                        new Comment
+                        {
+                            Author = user1,
+                            Title = "A great book!",
+                            Content = "Nothing to add."
+                        },
+                        new Comment
+                        {
+                            Author = user2,
+                            Title = "Superb",
+                            Content = "What should I say."
+                        },
+                        new Comment
+                        {
+                            Author = user3,
+                            Title = "Boring!",
+                            Content = "Cannot finish it!"
+                        }
+                    ]
+                });
+
+        var book2 = bookRepository
+           .Add(new Book
+                {
+                    Title = "The heretics of Dune",
+                    Author = "Frank Herbert",
+                    Comments =
+                    [
+                        new Comment
+                        {
+                            Author = user1,
+                            Title = "A great sequel!",
+                            Content = "Adding something would be too much."
+                        },
+                        new Comment
+                        {
+                            Author = user2,
+                            Title = "Not so great",
+                            Content = "I liked Dune more."
+                        },
+                        new Comment
+                        {
+                            Author = user3,
+                            Title = "Another boring book!",
+                            Content = "Cannot finish this book either!"
+                        }
+                    ]
+                });
+
+        user2
+           .BorrowedBooks
+           .Add(book1);
 
         await unitOfWork
            .CompleteAsync();
